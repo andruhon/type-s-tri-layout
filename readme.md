ce314c650bd01a6200176588312d9be78cb5bc86
Andrew Kondratev <andruhon@gmail.com>
2024-08-03 23:00:56 +1200

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Corne 3x5 VIAL layout
+
+Initial commit. No layouts other than basic yet.
+
+Building a 3x5 layout for my https://github.com/klouderone/cornev4promicroedition
+
+Use on your risk.
+
+My corne is using Pro Micro controller. It needs double tap on reset to switch to bootloader mode.
---
+++ rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = yes
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
