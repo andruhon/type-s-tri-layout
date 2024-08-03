374f7673a7660eca3911c71b125c60a518e6f24a
Andrew Kondratev <andruhon@gmail.com>
2024-08-04 00:06:56 +1200

    starting work on layout

--- keymap.c
+++ keymap.c
@@ -19,32 +19,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // BASE
     [0] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+                                KC_LALT, TL_LOWR, KC_SPC,           KC_ENT,  TL_UPPR,  KC_RALT
     ),
 
+    // LOWR
     [1] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    KC_HOME, KC_UP,   KC_END,   XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+                                KC_LGUI, _______, KC_SPC,           KC_ENT,  _______,  KC_RCTL
     ),
 
+    // UPPR
     [2] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+                                KC_LGUI, _______, KC_SPC,           KC_ENT,  _______,  KC_RCTL
     ),
 
+    // ADJUST (HOLD BOTH LOWR AND UPPR)
     [3] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+                                KC_LGUI, _______, KC_SPC,           KC_ENT,  _______,  KC_RCTL
     )
 };
 
--- readme.md
+++ readme.md
@@ -10,3 +10,15 @@ My corne is using Pro Micro controller. It needs double tap on reset to switch t
 
 ## Keyboard matrix
 ![Matrix](matrix.png)
+
+## Build
+cd to vial qmk `cd vial-qmk/` 
+`make crkbd:vial CONVERT_TO=promicro_rp2040`
+
+## Flash
+* Unplug keyboard from USB; 
+* Unplug TRRS or TRS cable; 
+* Plug one side to USB; 
+* My corne is using Pro Micro controller. It needs double tap on reset to switch to bootloader mode. 
+* Once it is in bootloader mode the keyboard becomes available as new drive. uf2 file can be copied into this drive. 
+* Repeat for another side.
