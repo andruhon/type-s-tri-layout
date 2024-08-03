77fe56a149260c1b373e0a571248e2658b478be0
Andrew Kondratev <andruhon@gmail.com>
2024-08-04 00:06:40 +1200

    Add autoshift

--- rules.mk
+++ rules.mk
@@ -2,6 +2,8 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
+AUTO_SHIFT_ENABLE 	= yes
+
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
