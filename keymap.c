c2d79ffefdb8f5a4d2bc24c575c7bd6f0a750a58
Andrew Kondratev <andruhon@gmail.com>
2024-08-05 01:31:31 +1200

    improve nav convenience

--- keymap.c
+++ keymap.c
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // LOWR
     [1] = LAYOUT_split_3x5_3(
         KC_ESC,  KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_PGUP,  KC_HOME, KC_UP,   KC_END,   KC_BSPC,
-        KC_GRAVE,KC_F4,   KC_F5,   KC_F6,   KC_F11,                             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_BSLS,
-        KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F12,                             KC_DEL,   KC_TAB,  KC_INS,  KC_APP,   KC_QUOT,
-                                KC_LALT, KC_LGUI, _______,          _______,  KC_ENT,  KC_RCTL
+        KC_GRAVE,KC_F4,   KC_F5,   KC_F6,   KC_F11,                             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,
+        KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F12,                             KC_DEL,   KC_TAB,  KC_INS,  KC_APP,   KC_ENT,
+                                KC_LALT, KC_LGUI, _______,          _______,  KC_RCTL,  XXXXXXX
     ),
 
     // UPPR (hold UPPR and N KC_BTN1 to drag and drop)
