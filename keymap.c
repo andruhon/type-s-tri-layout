3b99b1f3d088a8a77f789808bf064c47d3f2d596
Andrew Kondratev <andruhon@gmail.com>
2024-08-13 22:44:15 +1200

    some convenience improvements

--- keymap.c
+++ keymap.c
@@ -41,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // LOWR
     [1] = LAYOUT_split_3x5_3(
-        KC_ESC,  KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_PGUP,  KC_HOME, KC_UP,   KC_END,   KC_BSPC,
-        KC_GRAVE,KC_F4,   KC_F5,   KC_F6,   KC_F11,                             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,
-        KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F12,                             KC_DEL,   KC_TAB,  KC_INS,  KC_APP,   KC_LSFT,
-                                KC_LALT, KC_LGUI, _______,           _______,  KC_ENT,  KC_RCTL
+        KC_ESC,  KC_F7,   KC_F8,   KC_F9,   KC_F12,                             KC_PGUP,  KC_HOME, KC_UP,   KC_END,   KC_BSPC,
+        KC_GRAVE,KC_F4,   KC_F5,   KC_F6,   KC_F10,                             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,
+        KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F11,                             KC_DEL,   KC_TAB,  KC_INS,  KC_APP,   KC_LSFT,
+                                KC_LCTL, KC_LGUI, _______,           _______,  KC_ENT,  KC_RALT
     ),
 
     // UPPR (hold UPPR and N KC_BTN1 to drag and drop)
     [2] = LAYOUT_split_3x5_3(
-        KC_ESC,  KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                            KC_MINUS, KC_7,    KC_8,    KC_9,     KC_0,
-        KC_QUOT, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                            KC_EQUAL, KC_4,    KC_5,    KC_6,     KC_DOT,
+        KC_ESC,  KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                            KC_MINUS, KC_7,    KC_8,    KC_9,     KC_BSPC,
+        KC_QUOT, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                            KC_EQUAL, KC_4,    KC_5,    KC_6,     KC_0,
         KC_LSFT, KC_BSLS, KC_COMM, KC_DOT,  KC_BTN3,                            KC_BTN1,  KC_1,    KC_2,    KC_3,     KC_ACL0,
-                                KC_LALT, KC_LCTL, _______,           _______, KC_BTN3, KC_RALT
+                                KC_LALT, KC_SPC, _______,            _______,  KC_BTN3, KC_RCTL
     ),
     // use ACL0 for precise mouse movements
 
