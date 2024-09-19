f9472f37337a513878f9fab07680b58bda0610fd
Andrew Kondratev <andruhon@gmail.com>
2024-09-19 22:47:35 +1200

    make ctrl and alt inversion consistent on both lowr and uppr

--- keymap.c
+++ keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                            KC_MINUS, KC_7,    KC_8,    KC_9,     KC_BSPC,
         KC_QUOT, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                            KC_EQUAL, KC_4,    KC_5,    KC_6,     KC_0,
         KC_LSFT, KC_BSLS, KC_COMM, KC_DOT,  KC_BTN3,                            KC_BTN1,  KC_1,    KC_2,    KC_3,     KC_ACL0,
-                                KC_LALT, KC_SPC, _______,            _______,  KC_BTN3, KC_RCTL
+                                KC_LCTL, KC_SPC, _______,            _______,  KC_BTN3, KC_RALT
     ),
     // use ACL0 for precise mouse movements
 
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ADJUST (HOLD BOTH LOWR AND UPPR).
     // s, d, f, h, n, k and l are reserved personal macros (those hand-twisting combinations like WIN+SHIFT+S)
     [3] = LAYOUT_split_3x5_3(
-        KC_ESC,  DF(0),   DF(1),   DF(2),   KC_VOLU,                             KC_PSCR,  KC_CAPS, XXXXXXX, KC_LBRC,  XXXXXXX,
-        KC_TAB,XXXXXXX,XXXXXXX,XXXXXXX,KC_MUTE,                          XXXXXXX,  KC_GRAVE,XXXXXXX, XXXXXXX,  KC_RBRC,
-        KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_VOLD,                             KC_DEL,   KC_RGUI, KC_RALT, KC_RCTL,  KC_RSFT,
+        KC_ESC,    DF(0),   DF(1),   DF(2), KC_VOLU,                            KC_PSCR,  KC_CAPS, XXXXXXX, KC_LBRC,  XXXXXXX,
+        KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE,                            XXXXXXX,  KC_GRAVE,XXXXXXX, XXXXXXX,  KC_RBRC,
+        KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_VOLD,                            KC_DEL,   KC_RGUI, KC_RALT, KC_RCTL,  KC_RSFT,
                                 XXXXXXX, XXXXXXX, _______,           _______,  XXXXXXX,  XXXXXXX
     )
 };
