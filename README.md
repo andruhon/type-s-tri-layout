b840c8d15895eeab4ad4d146e1d871b728ef97a0
Andrew Kondratev <andruhon@gmail.com>
2025-03-18 22:11:49 +1300

    Upgrade to QMK 0.28.0

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 # TypeS Tri Layout
 34 Keys [Tri Layout](https://docs.qmk.fm/features/tri_layer) for QMK optimized for TypeScript and Java.
+I use it with Corne, but I can't see why this can't be used with other 36/34 keys keyboard.
 
-This layout has customizable keycaps project optimized for 3D printing https://github.com/andruhon/super-custom-keycaps
+This layout has [customizable keycaps](https://github.com/andruhon/super-custom-keycaps) project optimized for 3D printing  
+https://github.com/andruhon/super-custom-keycaps
 
 ![Corne with TypeS Tri Layout](keyboard.png)
 
 ## Setting up
+### Install QMK
 Install QMK following https://docs.qmk.fm/newbs_getting_started
+TLDR
 ```
+sudo pacman --needed --noconfirm -S git python-pip libffi
+python3 -m pip install --user qmk
+qmk setup -b 0.28.0
+see clonning into /home/user/yourdir/qmk_firmware
+```
+Note `/home/user/yourdir/qmk_firmware` above is a directory where the qmk_firmware was cloned.
+
+### Install Type-S Tri Layout
+```
+cd /home/user/yourdir/qmk_firmware
+cd keyboards/crkbd/keymaps/
+
+git clone git@github.com:andruhon/type-s-tri-layout.git typestrilayout
+
 qmk config user.keyboard=crkbd
-qmk config user.keymap=andruhon
-qmk new-keymap -kb crkbd
+qmk config user.keymap=typestrilayout
 ```
 
 ## Compile
 My controller is promicro_rp2040, so I have to use CONVERT_TO,
 to make sure the uf2 file produced.
 ```
-qmk compile -kb crkbd -km andruhon -e CONVERT_TO=promicro_rp2040
+qmk compile -kb crkbd -km typestrilayout -e CONVERT_TO=promicro_rp2040
 ```
 
 ## Flash
@@ -28,13 +45,16 @@ EE_HANDS is enabled, so both halves may be flashed.
 
 Flash left:
 ```
-qmk flash -kb crkbd -km andruhon -bl uf2-split-left -e CONVERT_TO=promicro_rp2040
+qmk flash -kb crkbd -km typestrilayout -bl uf2-split-left -e CONVERT_TO=promicro_rp2040
 ```
 
 Flash right:
 ```
-qmk flash -kb crkbd -km andruhon -bl uf2-split-right -e CONVERT_TO=promicro_rp2040
+qmk flash -kb crkbd -km typestrilayout -bl uf2-split-right -e CONVERT_TO=promicro_rp2040
 ```
+
+Be careful when plugging in and unplugging your keyboard halves during flashing, always 
+unplub USB cable first and only then remove TRS/TRRS cable connecing halves.
 
 ## Keyboard matrix and layout
 
--- keymap.c
+++ keymap.c
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // BASE (I personally ended up completely removing 3,3 KC_LALT and 7,3 KC_RCTL)
+    // BASE
     [0] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN,
@@ -43,15 +43,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_split_3x5_3(
         KC_ESC,  KC_F7,   KC_F8,   KC_F9,   KC_F12,                             KC_PGUP,  KC_HOME, KC_UP,   KC_END,   KC_BSPC,
         KC_GRAVE,KC_F4,   KC_F5,   KC_F6,   KC_F10,                             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,
-        KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F11,                             KC_DEL,   KC_TAB,  KC_INS,  KC_APP,   KC_LSFT,
+        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F11,                             KC_DEL,   KC_TAB,  KC_INS,  KC_APP,   KC_NO,
                                 KC_NO, KC_SPC, _______,           _______,  KC_ENT,  KC_NO
     ),
 
-    // RAISE (hold RAISE and N KC_BTN1 to drag and drop)
+    // RAISE
+    // (hold RAISE and N KC_BTN1 to mouse drag and drop, it's like you pinching something)
     [2] = LAYOUT_split_3x5_3(
-        KC_ESC,  KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                            KC_MINUS, KC_7,    KC_8,    KC_9,     KC_BSPC,
-        KC_QUOT, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                            KC_EQUAL, KC_4,    KC_5,    KC_6,     KC_0,
-        KC_LSFT, KC_BSLS, KC_COMM, KC_DOT,  KC_BTN3,                            KC_BTN1,  KC_1,    KC_2,    KC_3,     KC_ACL0,
+        KC_ESC,  MS_BTN2, MS_UP,   MS_BTN1, MS_WHLU,                            KC_MINUS, KC_7,    KC_8,    KC_9,     KC_BSPC,
+        KC_QUOT, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD,                            KC_EQUAL, KC_4,    KC_5,    KC_6,     KC_0,
+        MS_BTN3, KC_BSLS, KC_COMM, KC_DOT,  KC_NO,                              KC_BTN1,  KC_1,    KC_2,    KC_3,     MS_ACL0,
                                 KC_NO, KC_SPC, _______,            _______,  KC_ENT, KC_NO
     ),
     // use ACL0 for precise mouse movements
@@ -61,12 +62,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // middle row lambdas in TS and Java producing ()=>{} and ()->{} appropriately
     [3] = LAYOUT_split_3x5_3(
         KC_ESC,  KC_VOLD, KC_MUTE, KC_VOLU, KC_PAUSE,                           KC_PSCR,  KC_CAPS, KC_LBRC, KC_RBRC,  KC_BSPC,
-        KC_TAB,  KC_LPRN, KC_RPRN, KC_SPC,  KC_GRAVE,                           KC_EQUAL, KC_RABK, KC_LCBR, KC_RCBR,  KC_MINUS, 
+        KC_TAB,  KC_LPRN, KC_RPRN, KC_SPC,  KC_GRAVE,                           KC_EQUAL, KC_RABK, KC_LCBR, KC_RCBR,  KC_MINUS,
         KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_DEL,                             KC_ENT,   KC_RGUI, KC_RALT, KC_RCTL,  KC_RSFT,
                                 KC_NO, KC_SPC, _______,           _______,  KC_ENT,  KC_NO
     )
 };
 
+// // Uncomment this block if you need custom macros
 // bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //     if (record->event.pressed) {
 //         switch (keycode) {
